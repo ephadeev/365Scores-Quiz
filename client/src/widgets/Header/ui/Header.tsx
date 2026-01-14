927efc0a24fdd9b346883d4ca976fd229d39b4d8
ephadeev <itrex1991@hotmail.com>
2026-01-14 20:01:40 +0300

    implemented Header widget

---
+++ client/src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink} from 'react-router';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <nav className='blue-grey darken-3'>
+                <div className="nav-wrapper">
+                    <NavLink to='/' className='brand-logo center'>
+                        <span className='white blue-grey-text text-darken-3 header__logo'>365</span>
+                        <span className='header__logo'>Scores Quiz</span>
+                    </NavLink>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
