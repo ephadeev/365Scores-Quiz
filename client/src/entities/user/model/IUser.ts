1cee514e06af463c3c77a1b440a14e2dc2974299
ephadeev <itrex1991@hotmail.com>
2026-01-14 20:00:28 +0300

    added User entity

---
+++ client/src/entities/user/model/IUser.ts
@@ -0,0 +1,5 @@
+export interface IUser {
+    id: string | null;
+    currentQuestionId: number;
+    score: number;
+}
---
+++ client/src/entities/user/model/userSlice.ts
@@ -0,0 +1,51 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {IUser} from "./IUser";
+import {RootState} from "../../../shared/store/model/store";
+
+export interface UserState {
+    id: string | null;
+    currentQuestionId: number;
+    score: number;
+}
+
+const initialState: UserState = {
+    id: null,
+    currentQuestionId: 0,
+    score: 0
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUserId: (state, action: PayloadAction<string>) => {
+            state.id = action.payload;
+        },
+        incrementCurrentQuestionId: state => {
+            state.currentQuestionId += 1;
+        },
+        incrementScore: state => {
+            state.score += 1;
+        },
+        resetScore: state => {
+            state.score = initialState.score;
+
+        },
+        resetCurrentQuestionId: state => {
+            state.currentQuestionId = initialState.currentQuestionId;
+        }
+    }
+
+})
+
+export const getUserId = (state: RootState) => state.user.id;
+export const getCurrentQuestionId = (state: RootState) => state.user.currentQuestionId;
+export const getScore = (state: RootState) => state.user.score;
+export const {
+    setUserId,
+    incrementCurrentQuestionId,
+    incrementScore,
+    resetScore,
+    resetCurrentQuestionId
+} = userSlice.actions;
+export default userSlice.reducer;
