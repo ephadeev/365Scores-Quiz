38e23d4b1c6ff133ed579a9f96201139e3ffe3f2
ephadeev <itrex1991@hotmail.com>
2026-01-14 20:02:07 +0300

    created Icon component

---
+++ client/src/shared/ui/Icon/Icon.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from 'react';
+
+const Icon: FC<{kind: string, style?: string}> = ({kind, style}) => {
+    return (
+        <i className={`material-icons ${style}`}>{kind}</i>
+    );
+}
+
+export default Icon;
