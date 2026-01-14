fd689e8d1ba422d6cfd1b1ffb04edb80d4e53de0
ephadeev <itrex1991@hotmail.com>
2026-01-14 20:01:31 +0300

    implemented Footer widget

---
+++ client/src/widgets/Footer/ui/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='page-footer blue-grey darken-3'>
+            <div className="footer-copyright">
+                <div className="container">
+                    © 2026 All rights reserved.
+                    <a
+                        className="grey-text text-lighten-4 right"
+                        href="https://www.linkedin.com/in/evgeny-phadeev-0a639899/?locale=en_US"
+                        target='blank'
+                        rel='noopener noreferrer'>developed by ephadeev</a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
