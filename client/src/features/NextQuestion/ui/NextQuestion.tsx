b40489d1923b58c64275141034111b112ffb83a0
ephadeev <itrex1991@hotmail.com>
2026-02-06 18:15:55 +0300

    migrate NextQuestion into a separate component

---
+++ client/src/features/NextQuestion/ui/NextQuestion.tsx
@@ -0,0 +1,31 @@
+import React, {FC, memo} from 'react';
+import {
+    getSelectedOption,
+    setSelectedOption,
+    incrementCurrentQuestionId
+} from "../../../entities/quiz/model/quizSlice";
+import {useAppDispatch, useAppSelector} from "../../../shared/store/lib/reduxHooks";
+
+const NextQuestion: FC = memo(() => {
+    const dispatch = useAppDispatch();
+    const selectedOption = useAppSelector(getSelectedOption);
+
+    const handleNextQuestionClick = () => {
+        // trigger rendering of the next quiz
+        dispatch(incrementCurrentQuestionId());
+
+        // reset selected option id
+        dispatch(setSelectedOption(null));
+    }
+
+    return (
+        <button
+            className="waves-effect waves-light btn"
+            onClick={handleNextQuestionClick}
+            disabled={selectedOption === null}>
+            Next Question
+        </button>
+    )
+})
+
+export default NextQuestion;
