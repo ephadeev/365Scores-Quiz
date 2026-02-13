c1bbe73d82ce13519e0e160280085a02a75ce925
ephadeev <itrex1991@hotmail.com>
2026-02-14 00:50:27 +0300

    Fixed server's port for vercel serverless

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ server.use(cors({
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`JSON Server running on port ${port}`);
 });
